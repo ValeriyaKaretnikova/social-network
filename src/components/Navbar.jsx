42f933edea97bda9c0732c12216c6194f55d563e
Valeriya Karetnikova <valeriya.karetnikova@gmail.com>
2022-09-13 10:50:20 -0600

    css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
+import s from "./Navbar.module.css";
+
 const Navbar = () => {
   return (
-    <nav className="nav">
-    <div>
-      <a href="#c">Profile</a>
-    </div>
-    <div>
-      <a href="#c">Messages</a>
-    </div>
-    <div>
-      <a href="#c">News</a>
-    </div>
-    <div>
-      <a href="#c">Music</a>
-    </div>
-    <div>
-      <a href="#c">Settings</a>
-    </div>
-  </nav>
+    <nav className={s.nav}>
+      <div >
+        <a href="#c" className={`${s.item} ${s.active}`}>Profile</a>
+      </div>
+      <div className={s.item}>
+        <a href="#c" className={s.item}>Messages</a>
+      </div>
+      <div className={s.item}>
+        <a href="#c" className={s.item}>News</a>
+      </div>
+      <div className={s.item}>
+        <a href="#c" className={s.item}>Music</a>
+      </div>
+      <div className={s.item}>
+        <a href="#c" className={s.item}>Settings</a>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,17 @@
+import s from'./Profile.module.css';
+
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
     <div>
       <img src={require("../img/background.jpg")} alt="background car" />
     </div>
     <div>ava + descr</div>
-    <div>
+    <div className='posts'>
       My posts
-      <div>New post</div>
-      <div>post 1</div>
-      <div>post 2</div>
+      <div >New post</div>
+      <div className={s.item}>post 1</div>
+      <div className={s.item}>post 2</div>
     </div>
   </div>
   );
