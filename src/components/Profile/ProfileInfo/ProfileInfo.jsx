774fa95eade76be393dc595e624ef111ea5c2a3b
Valeriya Karetnikova <valeriya.karetnikova@gmail.com>
2022-09-20 13:48:03 -0600

    profile page background

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,12 +3,7 @@ import s from "./ProfileInfo.module.css";
 const ProfileInfo = (props) => {
   return (
     <div>
-      <div>
-        <img
-          src={require("../../../img/background.jpg")}
-          alt="background car"
-        />
-      </div>
+      <div className={s.background}></div>
       <div className={s.description}>ava + descr</div>
     </div>
   );
